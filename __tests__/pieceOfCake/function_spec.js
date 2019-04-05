eca16ed2535dc51dfb987e0b769eccb85c23ea89
Liu Xia <lxconan@gmail.com>
2019-04-05 18:54:44 +0800

    [liuxia] feat: add test for function. Chapter 3 Functions.

--- __tests__/pieceOfCake/function_spec.js
+++ __tests__/pieceOfCake/function_spec.js
@@ -25,4 +25,116 @@ describe('for function', () => {
 
     expect(greeting(sayHello, 'World')).toEqual(expected);
   });
+
+  it('should allow optional arguments for any function', () => {
+    function square(x) { return x * x; }
+
+    const expected = 36;
+    expect(expected).toEqual(square(6, 'Hello', 4));
+  });
+
+  it('should be undefined for not specified parameter', () => {
+    function minus(left, right) {
+      if (right === undefined) { return -left; }
+      return left - right;
+    }
+
+    const expectedForSingleArgument = -5;
+    const expectedForTwoArguments = 2;
+
+    expect(expectedForSingleArgument).toEqual(minus(5));
+    expect(expectedForTwoArguments).toEqual(minus(5, 3));
+  });
+
+  it('should specify default parameters', () => {
+    function power(base, exponent = 2) {
+      let result = 1;
+      for (let count = 0; count < exponent; count += 1) {
+        result *= base;
+      }
+      return result;
+    }
+
+    const expected = 16;
+    expect(expected).toEqual(power(4));
+  });
+
+  it('should not modify the original variable', () => {
+    // eslint-disable-next-line prefer-const
+    let guessIfIAmChanged = 'Origin';
+    function transferToAnotherWord(word) {
+      // eslint-disable-next-line no-param-reassign
+      word = 'Changed';
+      return word;
+    }
+
+    const returnValue = transferToAnotherWord(guessIfIAmChanged);
+
+    const expectedReturnValue = 'Changed';
+    const expectedWord = 'Origin';
+
+    expect(expectedReturnValue).toEqual(returnValue);
+    expect(expectedWord).toEqual(guessIfIAmChanged);
+  });
+
+  it('should modify the content of the variable', () => {
+    const person = {};
+    // eslint-disable-next-line no-shadow
+    function addName(person, name) {
+      // eslint-disable-next-line no-param-reassign
+      person.name = name;
+      return person;
+    }
+
+    const returnValue = addName(person, 'Bob');
+
+    const expectedName = 'Bob';
+    const expectedReturnValueName = 'Bob';
+
+    expect(expectedName).toEqual(person.name);
+    expect(expectedReturnValueName).toEqual(returnValue.name);
+  });
+
+  it('should capture local variables', () => {
+    function wrapValue() {
+      const localVariable = 'Hello';
+      return () => localVariable;
+    }
+
+    const actual = wrapValue()();
+    const expected = 'Hello';
+
+    expect(expected).toEqual(actual);
+  });
+
+  it('should change captured variable', () => {
+    let guessIfIAmChanged = 'Origin';
+
+    function wrapValue() {
+      return () => { guessIfIAmChanged = 'Changed'; };
+    }
+
+    wrapValue()();
+
+    const expected = 'Changed';
+
+    expect(expected).toEqual(guessIfIAmChanged);
+  });
+
+  it('should create some recursion trick', () => {
+    function findSolution(target) {
+      function find(current, history) {
+        // eslint-disable-next-line eqeqeq
+        if (current == target) { return history; }
+        if (current > target) { return null; }
+        return find(current + 5, `(${history} + 5)`)
+          || find(current * 3, `(${history} * 3)`);
+      }
+
+      return find(1, '1');
+    }
+
+    const expected = '(((1 * 3) + 5) * 3)';
+    expect(expected).toEqual(findSolution(24));
+  });
 });
